26498dd79c22c613fc158600f4869c527d2f1d66
kst164 <kartheek@tammana.org>
2023-11-25 00:10:36 +0530

    fix run.sh

--- code/Samples/builtins.arc
+++ code/Samples/builtins.arc
@@ -1,4 +1,7 @@
 fn main(): i32 {
     let x: [i32];
     push(x, 1);
+
+    let y: i32 = pop(x);
+    y = pop(x);
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
+echo "Compiling compiler"
 make
-./a.out $1
+echo "Converting test file $1 to C++"
+./a.out < $1
+echo "Compiling C++ file to executable"
 g++ a.cpp -lgmp
+echo "Running executable"
 ./a.out

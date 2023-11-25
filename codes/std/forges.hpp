1af9209255fda5a07b35f1a344a6cc97bc5a8b95
Prasham Walvekar <cs21btech11047@iith.ac.in>
2023-11-25 12:34:33 +0530

    added forges

--- codes/std/forges.hpp
+++ codes/std/forges.hpp
@@ -27,188 +27,232 @@
 // vector<int>                  - from Symmetric, Alternating
 // vector<vector<T>>            - from Matrix
 
-BigInt forge_BigInt(const std::string str) {
+BigInt forge_BigInt(const std::string str)
+{
     return BigInt(str);
 }
 
-BigInt forge_BigInt(int n) {
+BigInt forge_BigInt(int n)
+{
     return BigInt(n);
 }
 
-template<int N>
-Cyclic<N> forge_Cyclic(int n) {
+template <int N>
+Cyclic<N> forge_Cyclic(int n)
+{
     return Cyclic<N>(n);
 }
 
-template<int N>
-Symmetric<N> forge_Symmetric(std::vector<int> vec) {
+template <int N>
+Symmetric<N> forge_Symmetric(std::vector<int> vec)
+{
     return Symmetric<N>(vec);
 }
 
-template<int N>
-Symmetric<N> forge_Symmetric(const Alternating<N> alt) {
+template <int N>
+Symmetric<N> forge_Symmetric(const Alternating<N> alt)
+{
     return Symmetric<N>(alt.perm);
 }
 
-template<int N>
-Alternating<N> forge_Alternating(const std::vector<int> vec) {
+template <int N>
+Alternating<N> forge_Alternating(const std::vector<int> vec)
+{
     return Alternating<N>(vec);
 }
 
-template<int N>
-Alternating<N> forge_Alternating(const Symmetric<N> sym) {
+template <int N>
+Alternating<N> forge_Alternating(const Symmetric<N> sym)
+{
     auto ret = Alternating<N>(sym.perm);
     ret.check_alt();
     return ret;
 }
 
-template<int N>
-Dihedral<N> forge_Dihedral(const std::tuple<int, bool> tup) {
+template <int N>
+Dihedral<N> forge_Dihedral(const std::tuple<int, bool> tup)
+{
     return Dihedral<N>(std::get<0>(tup), std::get<1>(tup));
 }
 
-template<typename T>
-Matrix<T> forge_Matrix(const std::vector<std::vector<T>> vec) {
+template <typename T>
+Matrix<T> forge_Matrix(const std::vector<std::vector<T>> vec)
+{
     return Matrix<T>(vec);
 }
 
-template<typename T>
-Polynomial<T> forge_Polynomial(const std::vector<T> vec) {
+template <typename T>
+Polynomial<T> forge_Polynomial(const std::vector<T> vec)
+{
     return Polynomial<T>(vec);
 }
 
-Rational forge_Rational(const std::tuple<int, int> frac) {
+Rational forge_Rational(const std::tuple<int, int> frac)
+{
     return Rational(std::get<0>(frac), std::get<1>(frac));
 }
 
-Rational forge_Rational(int n) {
+Rational forge_Rational(int n)
+{
     return Rational(n);
 }
 
-Complex forge_Complex(const std::tuple<int, int> frac) {
+Complex forge_Complex(const std::tuple<int, int> frac)
+{
     return Complex(std::get<0>(frac), std::get<1>(frac));
 }
 
-Complex forge_Complex(const std::tuple<Rational, Rational> frac) {
+Complex forge_Complex(const std::tuple<Rational, Rational> frac)
+{
     return Complex(std::get<0>(frac), std::get<1>(frac));
 }
 
-Complex forge_Complex(const Rational frac) {
+Complex forge_Complex(const Rational frac)
+{
     return Complex(frac);
 }
 
-Complex forge_Complex(int n) {
+Complex forge_Complex(int n)
+{
     return Complex(n);
 }
 
-int32_t forge_int32_t(const BigInt big) {
+int32_t forge_int32_t(const BigInt big)
+{
     return big.as_i32();
 }
 
-template<int N>
-int32_t forge_int32_t(const Cyclic<N> cyc) {
+template <int N>
+int32_t forge_int32_t(const Cyclic<N> cyc)
+{
     return cyc.r;
 }
 
-int32_t forge_int32_t(const std::string str) {
+int32_t forge_int32_t(const std::string str)
+{
     return std::stoll(str);
 }
 
-double forge_double(const Rational rat) {
+double forge_double(const Rational rat)
+{
     auto r = mpq_class(rat.n, rat.d);
     return r.get_d();
 }
 
-double forge_double(const std::string str) {
+double forge_double(const std::string str)
+{
     return std::stod(str);
 }
 
-std::tuple<int, int> forge_tuple_int_int(const Rational rat) {
+std::tuple<int, int> forge_tuple_int_int(const Rational rat)
+{
     return std::make_tuple(rat.n.get_si(), rat.d.get_si());
 }
 
-std::tuple<Rational, Rational> forge_tuple_Rational_Rational(const Complex com) {
+std::tuple<Rational, Rational> forge_tuple_Rational_Rational(const Complex com)
+{
     return std::make_tuple(com.r, com.i);
 }
 
-std::tuple<int32_t, int32_t> forge_tuple_int32_t_int32_t(const Complex com) {
+std::tuple<int32_t, int32_t> forge_tuple_int32_t_int32_t(const Complex com)
+{
     return std::make_tuple(com.r.n.get_si(), com.i.n.get_si());
 }
 
-std::string forge_string(const int n) {
+std::string forge_string(const int n)
+{
     return std::to_string(n);
 }
-std::string forge_string(const unsigned int n) {
+std::string forge_string(const unsigned int n)
+{
     return std::to_string(n);
 }
 
-std::string forge_string(const double d) {
+std::string forge_string(const double d)
+{
     return std::to_string(d);
 }
 
-std::string forge_string(const BigInt big) {
+std::string forge_string(const BigInt big)
+{
     return big.to_string();
 }
 
-template<int N>
-std::string forge_string(const Cyclic<N> cyc) {
+template <int N>
+std::string forge_string(const Cyclic<N> cyc)
+{
     return std::to_string(cyc.r);
 }
 
-template<int N>
-std::string forge_string(const Symmetric<N> sym) {
+template <int N>
+std::string forge_string(const Symmetric<N> sym)
+{
     return sym.to_string();
 }
 
-template<int N>
-std::string forge_string(const Alternating<N> alt) {
+template <int N>
+std::string forge_string(const Alternating<N> alt)
+{
     return alt.to_string();
 }
 
-template<int N>
-std::string forge_string(Dihedral<N> dih) {
+template <int N>
+std::string forge_string(Dihedral<N> dih)
+{
     return dih.to_string();
 }
 
-std::string forge_string(Rational rat) {
+std::string forge_string(Rational rat)
+{
     return rat.to_string();
 }
 
-std::string forge_string(Complex com) {
+std::string forge_string(Complex com)
+{
     return com.to_string();
 }
 
-template<int N>
-std::vector<int> forge_vector_int(const Symmetric<N> sym) {
+template <int N>
+std::vector<int> forge_vector_int(const Symmetric<N> sym)
+{
     return sym.perm;
 }
 
-template<int N>
-std::vector<int> forge_vector_int(const Alternating<N> alt) {
+template <int N>
+std::vector<int> forge_vector_int(const Alternating<N> alt)
+{
     return alt.perm;
 }
 
-template<typename T>
-std::vector<std::vector<T>> forge_vector_vector_T(const Matrix<T> mat) {
+template <typename T>
+std::vector<std::vector<T>> forge_vector_vector_T(const Matrix<T> mat)
+{
     return mat.a;
 }
 
 // template<typename T>
-std::string forge_string(const vector<int> vec) {
+std::string forge_string(const std::vector<int> vec)
+{
     std::string out = "";
-    for(int i = 0; i < vec.size(); i++) {
-        if(i) out += ", ";
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (i)
+            out += ", ";
         out += std::to_string(vec[i]);
     }
+    return out;
 }
 
-
-std::string forge_string(const vector<unsigned int> vec) {
+std::string forge_string(const std::vector<unsigned int> vec)
+{
     std::string out;
-    for(int i = 0; i < vec.size(); i++) {
-        if(i) out += ", ";
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (i)
+            out += ", ";
         out += std::to_string(vec[i]);
     }
+    return out;
 }
 
 // test: call all forges
